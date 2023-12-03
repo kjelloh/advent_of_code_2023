bdc5a85b2ceb6325b1cc02e939c2ba9d2c95259e
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-03 20:06:14 +0100

    day03 bare bone in vscode on macOS

---
+++ day03/vscode/day03.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip> // E.g., std::quoted
+#include <string>
+#include <sstream> // E.g., std::istringstream, std::ostringstream
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <ranges> // E.g., std::subrange, std::view 
+#include <utility> // E.g., std::pair,..
+#include <algorithm> // E.g., std::find, std::all_of,...
+#include <numeric> // E.g., std::accumulate
+#include <limits> // E.g., std::numeric_limits
+
+extern char const* pTest;
+extern char const* pData;
+
+using Integer = int; // int: 843 253 387 long int: 32 762 853 787 275 long long int: 32 762 853 787 275
+using Result = Integer;
+using Answers = std::vector<std::pair<std::string,Result>>;
+
+using Model = std::vector<std::string>;
+
+Model parse(auto& in) {
+    Model result{};
+    std::string line{};
+    while (std::getline(in,line)) {
+        result.push_back(line);
+    }
+    return result;
+}
+
+namespace part1 {
+  Result solve_for(char const* pData) {
+      Result result{};
+      std::stringstream in{ pData };
+      auto data_model = parse(in);
+      return result;
+  }
+}
+
+namespace part2 {
+  Result solve_for(char const* pData) {
+      Result result{};
+      std::stringstream in{ pData };
+      auto data_model = parse(in);
+      return result;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Answers answers{};
+  for (auto const& answer : answers) {
+    std::cout << "\nanswer[" << answer.first << "] " << answer.second;
+  }
+  std::cout << "\n";
+  return 0;
+}
