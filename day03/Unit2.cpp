c2f2cc82f1ff24c2a810443f192a60091d87c696
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-03 17:14:33 +0100

    day03 part 1 example ok

--- day03/Unit2.cpp
+++ day03/Unit2.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <queue>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 
@@ -16,28 +17,31 @@ c_Solver::Result c_Solver::to_solution(std::istream& in) {
 	std::string entry{};
 	int row{};
 	using Position = std::pair<int,int>;
-	using Model = std::vector<std::pair<Position,std::string>>;
-    Model model{};
+	std::map<Position,std::string> possible_numbers{};
+	std::map<Position,char> symbols{};
 	while (std::getline(in,entry)) {
 		int col{};
 		this->logger->log(entry);
 		std::string number{};
 		for (char ch : entry) {
 			bool do_log_pos{false};
-			if (std::isdigit(ch)) {
+			if (ch<=' ') {
+				// skip white spaces (unclear why they are there though?)
+			}
+			else if (std::isdigit(ch)) {
 				number += ch;
 			}
 			else if (ch != '.') {
 				do_log_pos = true;
 				std::string symbol{};
 				symbol += ch;
-				this->logger->log("symbol:");
+				this->logger->log("symbol:'");
 				this->logger->append(symbol);
-				model.push_back({Position{row,col},symbol});
+				symbols[Position{row,col}] = ch;
 				if (!number.empty()) {
 					// end of number
-					model.push_back({Position{row,col-1},number});
-					this->logger->log("number:");
+					possible_numbers[Position{row,col-1}] = number;
+					this->logger->log("number:'");
 					this->logger->append(number);
 					number.clear();
 				}
@@ -45,36 +49,57 @@ c_Solver::Result c_Solver::to_solution(std::istream& in) {
 			else if (number.size()>0) {
 				do_log_pos = true;
 				// end of number
-				model.push_back({Position{row,col-1},number});
-				this->logger->log("number:");
+				possible_numbers[Position{row,col-1}] = number;
+				this->logger->log("number:'");
 				this->logger->append(number);
 				number.clear();
 			}
 			if (do_log_pos) {
-				this->logger->append(" at row:");
+				this->logger->append("' at row:");
 				this->logger->append(std::to_string(row));
 				this->logger->append(" col:");
 				this->logger->append(std::to_string(col-1));
 			}
 			++col;
 		}
-        ++row;
+		++row;
 	}
-	std::map<Position,int> valid_numbers{};
-	for (auto const& entry : model) {
-		if (entry.second.size() > 1) {
-			// A number
-			if (std::any_of(model.begin(),model.end(),[pos=entry.first](auto const& entry){
-				return (entry.second.size()==1); // Test = find any symbol
-			})) {
-				valid_numbers[entry.first]=std::stoi(entry.second);
+	std::map<Position,std::string> valid_numbers{};
+	std::queue<std::pair<Position,std::string>> q{};
+	for (auto const& candidate : possible_numbers) {q.push(candidate);}
+	while (!q.empty()) {
+		auto number = q.front();q.pop();
+		auto pos = number.first;
+		this->logger->log("pop "); this->logger->append(number.second);
+		this->logger->append(" [");
+		this->logger->append(std::to_string(number.first.first));
+		this->logger->append(",");
+		this->logger->append(std::to_string(number.first.second));
+		this->logger->append("]");
+		bool loop_again{true};
+		this->logger->log("Scanning:");
+		for (int row = pos.first-1;row<=pos.first+1 and loop_again;++row) {
+			for (int col = pos.second-number.second.size();col<=pos.second+1 and loop_again;++col) {
+				Position at{row,col};
+				this->logger->append("[");
+				this->logger->append(std::to_string(row));
+				this->logger->append(",");
+				this->logger->append(std::to_string(col));
+				this->logger->append("]");
+				if (symbols.count(at)>0) {
+					valid_numbers[pos]=number.second;
+			   		this->logger->log("valid!");
+                    loop_again = false;
+				}
 			}
 		}
 	}
 	auto result = std::accumulate(valid_numbers.begin(),valid_numbers.end(),Result{0},[this](auto acc,auto const& entry){
-		acc += entry.second;
-		this->logger->log(std::to_string(entry.second));
+		acc += std::stoi(entry.second);
+		this->logger->log(entry.second);
 		return acc;
 	});
+	this->logger->log("Answer:");
+    this->logger->append(std::to_string(result));
 	return result;
 }
