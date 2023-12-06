1174136efb0628bea4d5ae95c075c6d77e2fd8ad
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-06 10:37:10 +0100

    day06 part 2 parse ok (use 64 bit ints)

--- day06/day06.cpp
+++ day06/day06.cpp
@@ -21,15 +21,22 @@
 auto const NL = "\n";
 auto const NT = "\n\t";
 
-using Integer = int; // int: 843 253 387 long int: 32 762 853 787 275 long long int: 32 762 853 787 275
+using Integer = std::int64_t; // 16 bit int: ± 3.27 · 10^4, 32 bit int: ± 2.14 · 10^9, 64 bit int: ± 9.22 · 10^18
 using Result = Integer;
 using Answers = std::vector<std::pair<std::string,Result>>;
 
-using Model = std::vector<std::pair<Integer,Integer>>;
+using Part1Model = std::vector<std::pair<Integer,Integer>>;
+using Part2Model = std::pair<Integer,Integer>; 
+struct Model {
+  Part1Model p1{};
+  Part2Model p2{};
+};
 
 Model parse(auto& in) {
     Model result{};
+    std::vector<std::string> sp2{};
     for (int i=0;i<2;++i) {
+      sp2.push_back({});
       std::string line{};
       std::getline(in,line);
       std::cout << NL << "entry:" << std::quoted(line);
@@ -38,23 +45,29 @@ Model parse(auto& in) {
       for (int j=0;ss >> token;++j) {
         std::cout << NT << "token:" << std::quoted(token);
         if (j>0) {
+          sp2.back().append(token);
           if (i==0) {
             std::cout << j-1 << " first " << std::stoi(token);
-            result.push_back({});
-            result[j-1].first = std::stoi(token);
+            result.p1.push_back({});
+            result.p1[j-1].first = std::stoi(token);
           }
           else {
             std::cout << j-1 << " second " << std::stoi(token);
-            result[j-1].second = std::stoi(token);
+            result.p1[j-1].second = std::stoi(token);
           }
         }
       }      
     }
+    std::cout << NL << "sp2[0]:" << sp2[0] << std::flush;
+    std::cout << NL << "sp2[1]:" << sp2[1] << std::flush;
+
+    result.p2.first = std::stoll(sp2[0]);
+    result.p2.second = std::stoll(sp2[1]);
     return result;
 }
 
 namespace part1 {
-  Result solve_for(Model const& model) {
+  Result solve_for(Part1Model const& model) {
       // Result result{std::numeric_limits<Result>::max()};
       Result result{};
       std::vector<Result> candidate_count{};
@@ -78,9 +91,10 @@ namespace part1 {
 }
 
 namespace part2 {
-  Result solve_for(Model const& model) {
+  Result solve_for(Part2Model const& model) {
       // Result result{std::numeric_limits<Result>::max()};
       Result result{};
+      std::cout << NL << "part2 - race time:" << model.first << " record distance:" << model.second;
       return result;
   }
 }
@@ -95,8 +109,8 @@ int main(int argc, char *argv[])
       std::ifstream in{argv[i]};
       if (in) {
         auto model = parse(in);
-        answers.push_back({argv[i],part1::solve_for(model)}); // 57075758
-        answers.push_back({argv[i],part2::solve_for(model)});
+        // answers.push_back({argv[i],part1::solve_for(model.p1)}); // 57075758
+        answers.push_back({argv[i],part2::solve_for(model.p2)});
       }
       else answers.push_back({std::string{"Failed to open file "} + argv[i],-1});
     }
