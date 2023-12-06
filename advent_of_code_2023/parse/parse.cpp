8fe72053cf61bd8c8f472e0ef5f1305667295504
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-06 21:58:08 +0100

    Visual studio 2022 edits and adjustments

--- advent_of_code_2023/parse/parse.cpp
+++ advent_of_code_2023/parse/parse.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <iomanip> // E.g., std::quoted
 #include <string>
+#include <string_view>
 #include <sstream> // E.g., std::istringstream, std::ostringstream
 #include <vector>
 #include <set>
@@ -26,6 +27,19 @@
 auto const NL = "\n";
 auto const T = "\t";
 auto const NT = "\n\t";
+
+namespace coroutes {
+  // How can we parse with coroutines?
+}
+
+namespace ranges {
+  // How can we parse with ranges?
+}
+
+namespace streams {
+  // how can we parse with streams
+}
+
 
 template <int DAY>
 class Parser {
@@ -263,7 +277,12 @@ private:
     if (split.size() > 0) co_yield{ split };
   }
 public:
-  char const* input = R"()";
+  char const* input = R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)";
   using Model = std::vector<std::pair<std::string, std::pair<std::set<std::string>, std::set<std::string>>>>;
 
   Model parse(auto& in) {
@@ -306,11 +325,206 @@ public:
   }
 };
 
+template <>
+class Parser<5> {
+private:
+  using Integer = std::int64_t; // 16 bit int: ± 3.27 · 10^4, 32 bit int: ± 2.14 · 10^9, 64 bit int: ± 9.22 · 10^18
+
+  enum Category {
+    undefined
+    , seed
+    , soil
+    , fertilizer
+    , water
+    , light
+    , temperature
+    , humidity
+    , location
+    , unknown
+
+  };
+  struct Value {
+    Category category{ undefined };
+    Integer value{};
+    bool operator<(Value const& other) const { return (category == other.category) ? value < other.value : category < other.category; }
+  };
+  using Values = std::set<Value>;
+  struct MapEntry {
+    Integer target_begin{};
+    Integer source_begin{};
+    Integer length{};
+  };
+  using MapEntries = std::vector<MapEntry>;
+  class Map {
+  public:
+    Map(Category target_category, MapEntries entries)
+      : target_category{ target_category }
+      , entries{ entries } {}
+    Map() = default;
+    Values operator[](Integer source) const {
+      Values result{};
+      bool has_mapping{ false };
+      for (auto const& entry : entries) {
+        Integer offset = (source - entry.source_begin);
+        if (offset >= 0 and offset < entry.length) {
+          // defined mapping
+          has_mapping = true;
+          result.insert({ target_category,entry.target_begin + offset });
+          // std::cout << NT << "entry.target_begin:" << entry.target_begin << "+" << offset << "=" << entry.target_begin + offset;
+        }
+      }
+      if (!has_mapping) result.insert({ target_category,source });
+      return result;
+    }
+  private:
+    Category target_category;
+    MapEntries entries{};
+  };
+  using Maps = std::map<Category, Map>;
+  struct Model {
+    std::vector<Integer> seeds;
+    Maps maps;
+  };
+
+public:
+  char const* input = R"(seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4)";
+  Model parse(auto& in) {
+    Model result{};
+    std::string line{};
+    while (std::getline(in, line)) {
+      std::cout << NL << "parsing:" << std::quoted(line);
+      std::pair<Category, Category> mapping{ undefined,undefined };
+      if (line.starts_with("seeds:")) {
+        auto s = line.substr(std::strlen("seeds:") + 1);
+        std::cout << " as seeds " << std::quoted(s) << "-->";
+        std::istringstream ss{ s };
+        std::string n;
+        while (ss >> n) {
+          std::cout << " value:" << n;
+          result.seeds.push_back(std::stoll(n));
+        }
+      }
+      else if (line.starts_with("seed-to-soil map:")) mapping = { seed,soil };
+      else if (line.starts_with("soil-to-fertilizer map:")) mapping = { soil,fertilizer };
+      else if (line.starts_with("fertilizer-to-water map:")) mapping = { fertilizer,water };
+      else if (line.starts_with("water-to-light map:")) mapping = { water,light };
+      else if (line.starts_with("light-to-temperature map:")) mapping = { light,temperature };
+      else if (line.starts_with("temperature-to-humidity map:")) mapping = { temperature,humidity };
+      else if (line.starts_with("humidity-to-location map:")) mapping = { humidity,location };
+
+      if (mapping.first != undefined and mapping.second != undefined) {
+        MapEntries entries{};
+        while (std::getline(in, line)) {
+          std::cout << NT << "entry:" << line;
+          if (line.empty()) break;
+          MapEntry entry{};
+          if (std::istringstream{ line } >> entry.target_begin >> entry.source_begin >> entry.length) {
+            std::cout << " push";
+            entries.push_back(entry);
+          }
+        }
+        result.maps[mapping.first] = Map{ mapping.second,entries };
+      }
+    }
+    return result;
+  }
+  auto parse() {
+    std::istringstream in{ input };
+    return parse(in);
+  }
+};
+
+template <>
+class Parser<6> {
+public:
+  char const* input = R"(Time:      7  15   30
+Distance:  9  40  200)";
+  using Integer = std::int64_t; // 16 bit int: ± 3.27 · 10^4, 32 bit int: ± 2.14 · 10^9, 64 bit int: ± 9.22 · 10^18
+  using Result = Integer;
+
+  using Part1Model = std::vector<std::pair<Integer, Integer>>;
+  using Part2Model = std::pair<Integer, Integer>;
+  struct Model {
+    Part1Model p1{};
+    Part2Model p2{};
+  };
+
+  Model parse(auto& in) {
+    Model result{};
+    std::vector<std::string> sp2{};
+    for (int i = 0; i < 2; ++i) {
+      sp2.push_back({});
+      std::string line{};
+      std::getline(in, line);
+      std::cout << NL << "entry:" << std::quoted(line);
+      std::istringstream ss{ line };
+      std::string token{};
+      for (int j = 0; ss >> token; ++j) {
+        std::cout << NT << "token:" << std::quoted(token);
+        if (j > 0) {
+          sp2.back().append(token);
+          if (i == 0) {
+            std::cout << j - 1 << " first " << std::stoi(token);
+            result.p1.push_back({});
+            result.p1[j - 1].first = std::stoi(token);
+          }
+          else {
+            std::cout << j - 1 << " second " << std::stoi(token);
+            result.p1[j - 1].second = std::stoi(token);
+          }
+        }
+      }
+    }
+    std::cout << NL << "sp2[0]:" << sp2[0] << std::flush;
+    std::cout << NL << "sp2[1]:" << sp2[1] << std::flush;
+
+    result.p2.first = std::stoll(sp2[0]);
+    result.p2.second = std::stoll(sp2[1]);
+    return result;
+  }
+  auto parse() {
+    std::istringstream in{ input };
+    return parse(in);
+  }
+};
+
 int main(int argc, char* argv[])
 {
   std::cout << NL << "Called with argc=" << argc << " arguments";
   for (int i = 0; i < argc; ++i) {
     std::cout << NT << "argv[" << i << "] : " << std::quoted(argv[i]);
   }
-  auto model = Parser<2>{}.parse();
+  auto model = Parser<6>{}.parse();
 }
