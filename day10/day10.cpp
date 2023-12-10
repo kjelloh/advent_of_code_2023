4927704c6fd1cb150c3986dcd20148e5be475aac
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-10 15:46:36 +0100

    day10 part 1 take 2 - realized we need to search for the "start" that is actually connected into a loop

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -61,189 +61,6 @@ void printModel(Model const& model) {
 }
 
 namespace part1 {
-  /*
-  So, todays puzzle is a circular graph problem. We are to find this loop in the graph.
-  And then the answer we are looking for is where the half way point is in the loop.
-
-  Our input seems to define edges? So the nodes themselves are implicit.
-
-  First we need to represent the inout data as a graph.
-
-  Then we can split this graph into its connected components.
-
-  one of these connected components is a loop so we need to find that one.
-
-  Now the problem is "just" to find the length of this loop and half it to get our answer.
-
-  */
-
-  class Graph {
-  private:
-    int V; // number of vertices
-    std::map<int,std::set<int>> adj{}; // adjacency list
-
-  public:
-    class Edge {
-    public:
-      int src; /**< The source vertex of the edge. */
-      int dest; /**< The destination vertex of the edge. */
-
-      /**
-       * @brief Constructs an Edge object with the given source and destination vertices.
-       * @param src The source vertex of the edge.
-       * @param dest The destination vertex of the edge.
-       */
-      Edge(int src, int dest) : src(src), dest(dest) {}
-    };
-
-    using Edges = std::vector<Edge>;
-
-    void addEdge(int v, int w) {
-      adj[v].insert(w);
-      adj[w].insert(v);
-    }
-
-    void addEdge(const Edge& edge) {
-      std::cout << NL << "addEdge" << std::flush;
-      std::cout << " " << edge.src << " -> " << edge.dest << std::flush;
-      addEdge(edge.src, edge.dest);
-    }
-
-    std::set<int> const& getAdjacentVertices(int v) const {
-      return adj.at(v);
-    }
-
-    int getVertexCount() const {
-      return adj.size();
-    }
-  };
-
-  class CC {
-  private:
-    std::vector<bool> marked; // marked[v] = has vertex v been marked?
-    std::vector<int> id; // id[v] = id of connected component containing v
-    std::vector<int> size; // size[id] = number of vertices in component id
-    int count; // number of connected components
-
-  public:
-    CC(const Graph& graph) {
-      marked.resize(graph.getVertexCount(), false);
-      id.resize(graph.getVertexCount());
-      size.resize(graph.getVertexCount());
-      count = 0;
-
-      for (int v = 0; v < graph.getVertexCount(); ++v) {
-        if (!marked[v]) {
-          dfs(graph, v);
-          ++count;
-        }
-      }
-    }
-
-    bool connected(int v, int w) const {
-      return id[v] == id[w];
-    }
-
-    int getCount() const {
-      return count;
-    }
-
-    int getId(int v) const {
-      return id[v];
-    }
-
-    int getSize(int id) const {
-      return size[id];
-    }
-
-  private:
-    void dfs(const Graph& graph, int v) {
-      marked[v] = true;
-      id[v] = count;
-      ++size[count];
-
-      for (int w : graph.getAdjacentVertices(v)) {
-        if (!marked[w]) {
-          dfs(graph, w);
-        }
-      }
-    }
-  };
-
-  class Cycle {
-  private:
-    std::vector<bool> marked{}; // marked[v] = has vertex v been visited?
-    bool has_cycle{false}; // does the graph have a cycle?
-
-  public:
-    Cycle(const Graph& graph) {
-      marked.resize(graph.getVertexCount(), false);
-
-      for (int v = 0; v < graph.getVertexCount(); ++v) {
-        if (!marked[v]) {
-          dfs(graph, v, v);
-        }
-      }
-    }
-
-    bool hasCycle() const {
-      return has_cycle;
-    }
-
-  private:
-    void dfs(const Graph& graph, int v, int u) {
-      marked[v] = true;
-      for (int w : graph.getAdjacentVertices(v)) {
-        if (!marked[w]) {
-          dfs(graph, w, v);
-        } else if (w != u) {
-          has_cycle = true;
-        }
-      }
-    }
-  };
-
-class Paths {
-private:
-  std::vector<bool> marked; // marked[v] = is there a path from source to v?
-  std::vector<int> edgeTo; // edgeTo[v] = last edge on path from source to v
-  int source; // source vertex
-
-public:
-  Paths(const Graph& graph, int source) : marked(graph.getVertexCount(), false), edgeTo(graph.getVertexCount()), source(source) {
-    dfs(graph, source);
-  }
-
-  bool hasPathTo(int v) const {
-    return marked[v];
-  }
-
-  std::vector<int> pathTo(int v) const {
-    std::cout << NL << "pathTo" << std::flush;
-    if (!hasPathTo(v)) {
-      return {}; // No path exists
-    }
-
-    std::vector<int> path;
-    for (int x = v; x != source; x = edgeTo[x]) {
-      path.push_back(x);
-    }
-    path.push_back(source);
-    std::reverse(path.begin(), path.end());
-    return path;
-  }
-
-private:
-  void dfs(const Graph& graph, int v) {
-    marked[v] = true;
-    for (int w : graph.getAdjacentVertices(v)) {
-      if (!marked[w]) {
-        edgeTo[w] = v;
-        dfs(graph, w);
-      }
-    }
-  }
-};
 
 class Delta {
   public:
@@ -256,6 +73,10 @@ class Delta {
      * @param delta_y The change in y-coordinate representing south/north.
      */
     Delta(int delta_x, int delta_y) : delta_x(delta_x), delta_y(delta_y) {}
+
+    bool operator==(const Delta& other) const {
+      return delta_x == other.delta_x && delta_y == other.delta_y;
+    }
   };
 
   class Position {
@@ -290,171 +111,133 @@ class Delta {
 
     
   };
-  using Positions = std::vector<Position>;
 
-  using MapEdge = std::pair<Position, Delta>;
-  using MapEdges = std::vector<MapEdge>;
+  using Positions = std::vector<Position>;
 
   const Delta EAST(1, 0);
   const Delta WEST(-1, 0);
   const Delta SOUTH(0, 1);
   const Delta NORTH(0, -1);
 
-  MapEdges to_map_edges(Position pos, char symbol) {
-    std::cout << NL << "to_map_edges" << std::flush;
-    MapEdges result{};
-    // Define an edge as going in a direction from the position of the symbol.
-    // So 'L' defines one edge to north of pos and one edge east of pos.
+  struct Walker {
+    Position position;
+    Delta direction;
+  };
+
+  /*
+    The pipes are arranged in a two-dimensional grid of tiles:
+
+    | is a vertical pipe connecting north and south.
+    - is a horizontal pipe connecting east and west.
+    L is a 90-degree bend connecting north and east.
+    J is a 90-degree bend connecting north and west.
+    7 is a 90-degree bend connecting south and west.
+    F is a 90-degree bend connecting south and east.
+    . is ground; there is no pipe in this tile.
+    S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+  */
+  const std::string SYMBOLS{"|-LJ7FS."};
+
+  Walker move(Walker const& walker, char symbol) {
+    Walker result{ walker };
+    bool valid_move = false;
     switch (symbol) {
-      case '|':
-        result.push_back({ pos, NORTH });
-        result.push_back({ pos, SOUTH });
-        break;
-      case '-':
-        result.push_back({ pos, EAST });
-        result.push_back({ pos, WEST });
-        break;
-      case 'L':
-        result.push_back({ pos, NORTH });
-        result.push_back({ pos, EAST });
-        break;
-      case 'J':
-        result.push_back({ pos, WEST});
-        result.push_back({ pos, NORTH });
-        break;
-      case '7':
-        result.push_back({ pos, WEST});
-        result.push_back({ pos, SOUTH });
-        break;
-      case 'F':
-        result.push_back({ pos, SOUTH });
-        result.push_back({ pos, EAST});
-        break;
-      case '.':
-        break;
-      default:
-        break;
+    case '|':
+      if (walker.direction == NORTH || walker.direction == SOUTH) {
+        result.position = walker.position + walker.direction;
+      }
+      else valid_move = false;
+      break;
+    case '-':
+      if (walker.direction == EAST || walker.direction == WEST) {
+        result.position = walker.position + walker.direction;
+      }
+      break;
+    case 'L':
+      if (walker.direction == SOUTH) {
+        result.direction = EAST;
+        result.position = walker.position + result.direction;
+      }
+      else if (walker.direction == WEST) {
+        result.direction = NORTH;
+        result.position = walker.position + result.direction;
+      }
+      break;
+    case 'J':
+      if (walker.direction == SOUTH) {
+        result.direction = WEST;
+        result.position = walker.position + result.direction;
+      }
+      else if (walker.direction == EAST) {
+        result.direction = NORTH;
+        result.position = walker.position + result.direction;
+      }
+      break;
+    case '7':
+      if (walker.direction == NORTH) {
+        result.direction = WEST;
+        result.position = walker.position + result.direction;
+      }
+      else if (walker.direction == EAST) {
+        result.direction = SOUTH;
+        result.position = walker.position + result.direction;
+      }
+      break;
+    case 'F':
+      if (walker.direction == NORTH) {
+        result.direction = EAST;
+        result.position = walker.position + result.direction;
+      }
+      else if (walker.direction == EAST) {
+        result.direction = SOUTH;
+        result.position = walker.position + result.direction;
+      }
+      break;
+    case '.':
+      break;
+    default:
+      std::cout << NL << "Unknown symbol: " << symbol;
+      break;
     }
+    if (!valid_move) std::cout << NL << "Invalid move " << symbol << " at position " << walker.position.x << "," << walker.position.y << " with direction " << walker.direction.delta_x << "," << walker.direction.delta_y << " and symbol " << symbol;
     return result;
   }
-
-  int to_vertex(Position const& pos,Positions& positions) {
-    std::cout << NL << "to_vertex"  << std::flush;
-    auto iter = std::find(positions.begin(), positions.end(), pos);
-    if (iter == positions.end()) {
-      std::cout << NL << "new position" << std::flush;
-      positions.push_back(pos);
-      return positions.size() - 1; // return index of new position
-    }
-    std::cout << NL << "existing position" << std::flush;
-    return std::distance(positions.begin(), iter);
-  }
-
-  int to_vertex(Position const& pos,Positions const& positions) {
-    std::cout << NL << "to_vertex"  << std::flush;
-    auto iter = std::find(positions.begin(), positions.end(), pos);
-    if (iter == positions.end()) {
-      throw std::runtime_error("vertex not found");
-    }
-    std::cout << NL << "existing position" << std::flush;
-    return std::distance(positions.begin(), iter);
-  }
-
-  Position to_position(int vertex, Positions const& positions) {
-    if (vertex < positions.size()) return positions[vertex];
-    throw std::runtime_error("vertex out of range");
-  }
-
-  std::pair<Graph,Positions> to_graph_and_positions(Model const& model) {
-    std::cout << NL << "to_graph";
-    Graph graph{};
-    Positions positions{};
-    for (int y = 0; y < model.size(); y++) {
-      for (int x = 0; x < model[y].size(); x++) {
-        Position pos(x, y); // Create position from row and col
-        std::cout << NL << "pos (" << pos.x << "," << pos.y << ")" << std::flush;
-        auto symbol = model[y][x];
-        auto map_edges = to_map_edges(pos,symbol);
-        for (auto const& [pos,delta] : map_edges) {
-          if (pos.x > 0 and pos.y > 0) {
-            std::cout << NL << "map_edge (" << pos.x << "," << pos.y << ") (" << delta.delta_x << "," << delta.delta_y << ")" << std::flush;
-            auto src = pos;
-            auto dest = Position(src.x + delta.delta_x, src.y + delta.delta_y);
-            auto edge = Graph::Edge(to_vertex(src,positions), to_vertex(dest,positions));
-            graph.addEdge(edge); 
-          }          
-        }
-      }
-    }
-    return {graph,positions};
-  }
-
-  void print_graph(Graph const& graph,Positions const& positions) {
-    std::cout << NL << "Graph:";
-    for (int v = 0; v < graph.getVertexCount(); v++) {
-      auto pos = to_position(v,positions);
-      std::cout << NL << "Vertex " << v << " position : (col:" << pos.x << ",row:" << pos.y << ")";
-      auto adjacent_vertices = graph.getAdjacentVertices(v);
-      for (auto const& adjacent_vertex : adjacent_vertices) {
-        auto adjacent_pos = to_position(adjacent_vertex,positions);
-        std::cout << NT << "Adjacent vertex " << adjacent_vertex << " (" << adjacent_pos.x << "," << adjacent_pos.y << ")";
-      }
-    }
-  }
-
-  int dfs(Graph const& graph, int current_vertex, int start_vertex, std::vector<bool>& visited) {
-    visited[current_vertex] = true;
-    int max_steps = 0;
-    auto adjacent_vertices = graph.getAdjacentVertices(current_vertex);
-    for (auto const& adjacent_vertex : adjacent_vertices) {
-      if (!visited[adjacent_vertex]) {
-        int steps = dfs(graph, adjacent_vertex, start_vertex, visited);
-        max_steps = std::max(max_steps, steps + 1);
-      } else if (adjacent_vertex == start_vertex) {
-        max_steps = std::max(max_steps, 1);
-      }
-    }
-    visited[current_vertex] = false;
-    return max_steps;
-  }  
 
   Result solve_for(Model& model) {
     Result result{};
     std::cout << NL << "Solver for part 1";
-    auto start = Position(0, 0);
-    int width = model[0].size();
-    int height = model.size();
-    // Find the start vertex
-    // NOTE: For my input the start vertex is (0,0)!
-    // But in the examples it is a bit more complicated...
-    // Anyhow...
-    bool found_start = false;
-    for (int distance = 0; distance < width + height;++distance and !found_start) {
-      for (int x = 0; x <= std::min(width,distance) and !found_start; ++x) {
-        int y = std::min(height,std::abs(distance - x));
+    
+    // find the start position
+    Position start_position{0,0};
+    int min_distance = std::numeric_limits<int>::max();
+    for (int y = 0; y < model.size() and y < min_distance; ++y) {
+      for (int x = 0; x < model[y].size() and x+y < min_distance; ++x) {
         if (model[y][x] == 'S') {
           model[y][x] = 'F';
-          // repair example map
         }
         if (model[y][x] == 'F') {
-          std::cout << NL << "Found start vertex at (" << x << "," << y << ")" << std::flush;
-          start = Position(x, y);
-          found_start = true;
+          int distance = std::abs(x) + std::abs(y);
+          if (distance < min_distance) {
+            min_distance = distance;
+            start_position = Position(x, y);
+          }
         }
       }
+      std::cout << NL << "start_position: " << start_position.x << "," << start_position.y;
+      Walker walker{ start_position, EAST };
+      do {
+        char symbol = model[walker.position.y][walker.position.x];
+        walker = move(walker, symbol);
+        std::cout << NL << "walker: " << walker.position.x << "," << walker.position.y << " with direction " << walker.direction.delta_x << "," << walker.direction.delta_y;
+      } while (walker.position != start_position);
     }
-    // transform the model into a graph
-    auto const& [graph, positions] = to_graph_and_positions(model);
-    print_graph(graph, positions);
-    std::vector<bool> visited(graph.getVertexCount(), false);
-    std::cout << NL << "graph has cycle" << std::flush;
-    try {
-      int max_steps = dfs(graph, 0, 0, visited); // max steps to loop
-      std::cout << NL << "max steps : " << max_steps << std::flush;
-      result = max_steps / 2;
-    } catch (std::runtime_error const& e) {
-      std::cout << NL << "runtime error : " << e.what() << std::flush;
-    }
+    
+    // walk the "pipe" clockwise until we are back at start
+    struct State {
+      Position position;
+      Delta direction;
+    };
+    
     return result;
   }
 }
