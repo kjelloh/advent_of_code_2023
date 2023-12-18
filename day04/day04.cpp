8c2f1e1831c0f3326b75ad620598d3e50a8a2b8c
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-18 20:29:12 +0100

    day04 - restarted with template and removed visual studio project file

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -1,24 +1,8 @@
-// Guard for incompatible compiler and c++ library, see https://en.cppreference.com/w/cpp/coroutine
-#if (__cpp_impl_coroutine	< 201902L) // 	(C++20)	Coroutines (compiler support)
-  // For gnu gcc support see https://gcc.gnu.org/wiki/cxx-coroutines
-// #error insufficient coroutine support by compiler
-#endif
-#if (__cpp_lib_coroutine	< 201902L) // 	(C++20)	Coroutines (library support)
-// For gnu/gcc libstdc++ support status see https://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html#status.iso.2020
-// #error insufficient coroutine support by c++ library
-#else
-#include <coroutine>
-#endif
-#if (__cpp_lib_generator < 202207L) //	(C++23)	std::generator: synchronous coroutine generator for ranges
-// #error C++ library does not support std::generator
-#else
-#include <generator>
-#endif
-
-
+#include <cctype>
 #include <iostream>
 #include <iomanip> // E.g., std::quoted
 #include <string>
+#include <string_view>
 #include <sstream> // E.g., std::istringstream, std::ostringstream
 #include <vector>
 #include <set>
@@ -33,125 +17,96 @@
 #include <numeric> // E.g., std::accumulate
 #include <limits> // E.g., std::numeric_limits
 #include <fstream>
-#include <experimental/generator> // supported by visual studio 2022 17.8.2 with project setting/compiler switch /std:c++latest
+// #include <experimental/generator> // supported by visual studio 2022 17.8.2 with project setting/compiler switch /std:c++latest
 #include <format>
+#include <optional>
+#include <regex>
 
-std::experimental::generator<std::string> lines(auto& in)
-{
-  std::string line{};
-  while (std::getline(in, line)) {
-    std::cout << "\nlines - " << line;
-    co_yield{ line };
-  }
-}
 
-std::experimental::generator<std::string> tokens(auto& token, char delimiter) {
-  std::string split{};
-  for (char ch : token) {
-    if (ch == delimiter) {
-      co_yield{ split };
-      split.clear();
-    }
-    else {
-      split += ch;
-    }
-  }
-  if (split.size()>0) co_yield{ split };
-}
+char const* example = R"()";
 
 auto const NL = "\n";
 auto const T = "\t";
 auto const NT = "\n\t";
 
-using Integer = int; // int: 843 253 387 long int: 32 762 853 787 275 long long int: 32 762 853 787 275
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
 using Result = Integer;
-using Answers = std::vector<std::pair<std::string,Result>>;
+using Answer = std::pair<std::string, Result>;
+using Answers = std::vector<Answer>;
+struct Solution {
+  Answers part1{};
+  Answers part2{};
+};
 
-using Model = std::vector<std::pair<std::string,std::pair<std::set<std::string>,std::set<std::string>>>>;
+using Model = std::vector<std::string>;
 
 Model parse(auto& in) {
-    Model result{};
-    std::string line{};
-    for (auto const& line : lines(in)) {
-      result.push_back({});
-      std::cout << NL << std::quoted(line);
-      for (auto const& [index,token] : std::views::enumerate(tokens(line,':'))) {
-        switch (index) {
-        case 0: {
-          // card x
-          std::cout << NT << std::quoted(token);
-          result.back().first = token;
-        } break;
-        default: {
-          std::set<int> numbers{};
-          for (auto const& [index,token] : std::views::enumerate(tokens(token, '|'))) {
-            std::cout << NT << T << std::quoted(token);
-            for (auto const& number : tokens(token, ' ')) {
-              if (number.size()>0) {
-                std::cout << NT << T << T << number;
-                switch (index) {
-                case 0: result.back().second.first.insert(number); break;
-                case 1: result.back().second.second.insert(number); break;
-                default: std::cerr << NL << "Failed to parse input - More than one set of numbers found in card entry";
-                }
-              }
-            }
-          }
-        } break;
-        }
-      }
-    }
-    return result;
+  Model result{};
+  std::string line{};
+  while (std::getline(in,line)) {
+    result.push_back(line);
+  }
+  return result;
 }
 
 namespace part1 {
-  Result solve_for(auto& in) {
-      Result result{};
-      auto model = parse(in);
-        result = std::accumulate(model.begin(), model.end(), Result{ 0 }, [](auto acc, auto const& entry) {
-          std::set<std::string> intersection{};
-          std::set_intersection(
-              entry.second.first.begin(), entry.second.first.end()
-            , entry.second.second.begin(), entry.second.second.end()
-            , std::inserter(intersection, intersection.begin()));
-          std::cout << NL << T << "Intersection " << entry.first << " [";
-          auto result = std::accumulate(intersection.begin(), intersection.end(), Result{ 0 }, [](auto acc, auto const& entry) {
-            std::cout << " " << entry;
-            acc = (acc==0)?1:acc*2;
-            return acc;
-            });
-          std::cout << "] ==> Points:" << result;
-          acc += result;
-          return acc;
-        });
-      return result;
+  Result solve_for(Model& model) {
+    Result result{};
+    std::cout << NL << NL << "part1";
+    return result;
   }
 }
 
 namespace part2 {
-  Result solve_for(auto& in) {
-      Result result{};
-      auto model = parse(in);
-      return result;
+  Result solve_for(Model& model) {
+    Result result{};
+    std::cout << NL << NL << "part2";
+    return result;
   }
 }
 
 int main(int argc, char *argv[])
 {
-  Answers answers{};
+  Solution solution{};
   std::cout << NL << "argc : " << argc;
-  for (int i=0;i<argc;++i) {
+  Answer part1_answer{ "Failed to obtain any input",0 };
+  Answer part2_answer{ "Failed to obtain any input",0 };
+  if (argc == 1) {
+    std::cout << NL << "no data file provided ==> WIll use hard coded example input";
+    std::istringstream in{ example };
+    auto model = parse(in);
+    part1_answer = { "Example",part1::solve_for(model) };
+    part2_answer = { "Example",part2::solve_for(model) };
+    solution.part1.push_back(part1_answer);
+    solution.part2.push_back(part2_answer);
+  }
+  else for (int i = 0; i < argc; ++i) {
     std::cout << NL << "argv[" << i << "] : " << std::quoted(argv[i]);
-    if (i>0) {
-      std::ifstream in{argv[i]};
-      if (in) answers.push_back({argv[i],part1::solve_for(in)});
-      else answers.push_back({std::string{"Failed to open file "} + argv[i],-1});
+    if (i > 0) {
+      std::ifstream in{ argv[i] };
+      if (in) {
+        auto model = parse(in);
+        part1_answer = { argv[i],part1::solve_for(model) };
+        part2_answer = { argv[i],part2::solve_for(model) };
+      }
+      else {
+        part1_answer = { std::string{"Failed to open file "} + argv[i],-1 };
+        part2_answer = { std::string{"Failed to open file "} + argv[i],-1 };
+      }
+      solution.part1.push_back(part1_answer);
+      solution.part2.push_back(part2_answer);
     }
   }
-  std::cout << "\n\n------------ REPORT----------------";
-  for (auto const& answer : answers) {
-    if (answer.second!=0) std::cout << "\nanswer[" << answer.first << "] " << answer.second;
-    else std::cout << "\nanswer[" << answer.first << "] " << " NO OPERATION ";
+  std::cout << NL << NL << "------------ REPORT----------------";
+  std::cout << NL << "<Part 1>";
+  for (auto const& answer : solution.part1) {
+    if (answer.second != 0) std::cout << NT << "answer[" << answer.first << "] " << answer.second;
+    else std::cout << NT << "answer[" << answer.first << "] " << " NO OPERATION ";
+  }
+  std::cout << NL << "<Part 2>";
+  for (auto const& answer : solution.part2) {
+    if (answer.second != 0) std::cout << NT << "answer[" << answer.first << "] " << answer.second;
+    else std::cout << NT << "answer[" << answer.first << "] " << " NO OPERATION ";
   }
   std::cout << "\n\n";
   return 0;
