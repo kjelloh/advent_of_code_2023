36315a061e193b56702f079ace99c1ec4ad491a9
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-20 16:58:40 +0100

    day20 part 1 example - another rabbit hole trying to hash a map of modules, each being a variant, to get a set of modules to detect a seen state. Too complicated!

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -6,6 +6,7 @@
 #include <sstream> // E.g., std::istringstream, std::ostringstream
 #include <vector>
 #include <set>
+#include <unordered_set>
 #include <map>
 #include <stack>
 #include <queue>
@@ -21,6 +22,7 @@
 #include <format>
 #include <optional>
 #include <regex>
+#include <functional>
 
 
 char const* example = R"(broadcaster -> a, b, c
@@ -107,6 +109,20 @@ namespace part1 {
   using Signal = std::optional<bool>;
   using Cache = std::map<std::string,Signal>;
 
+  template <typename T>
+  struct hash {};
+
+  template <>
+  struct hash<Cache> {
+    size_t operator()(const Cache& cache) const {
+      size_t seed = 0;
+      for (const auto& entry : cache) {
+        seed ^= std::hash<std::string>{}(entry.first) ^ (std::hash<Signal>{}(*entry.second) << 1);
+      }
+      return seed;
+    }
+  };
+
   // Flip-flop modules (prefix %) 
   //   are either on or off; they are initially off. If a flip-flop module receives a high pulse, it is ignored and nothing happens. 
   //   However, if a flip-flop module receives a low pulse, it flips between on and off. 
@@ -140,18 +156,54 @@ namespace part1 {
   namespace detail {
     template <typename F>
     struct Module {
-      std::map<std::string,Module&> destinations{};
       Cache cache{}; // initially all false / low
       bool state{ LOW };
       F op{};
-      Module const& act_on(std::string name,Signal pulse) {
+      Signal act_on(std::string name,Signal pulse) {
         cache[name] = pulse;
-        for (auto const& [name,destination] : destinations) {
-          destination.act_on(name,op(cache,state,pulse));
-        }
+        return op(cache,state,pulse);
+      }
+      size_t hash_value() const {
+        size_t type_hash = std::hash<std::string_view>{}(typeid(F).name());
+        size_t cache_hash = hash<Cache>{}(cache);
+        size_t state_hash = std::hash<bool>{}(state);
+        return type_hash ^ (cache_hash + state_hash + 0x9e3779b9 + (type_hash << 6) + (type_hash >> 2));
       }
     };
-  }
+  } // namespace detail
+  template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+  template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;   
+  using Module = std::variant<detail::Module<decltype(broadcast)>,detail::Module<decltype(flip_flop)>,detail::Module<decltype(conjunction)>>;
+  using Modules = std::map<std::string,Module>;
+
+  template<>
+  struct hash<Module> {
+    size_t operator()(const Module& module) const {
+      return std::visit([](const auto& arg) {
+        using T = std::decay_t<decltype(arg)>;
+        size_t type_hash = std::hash<std::string_view>{}(typeid(T).name());
+        size_t value_hash = arg.hash_value(); // Assuming each Module type has a hash_value() method
+        return type_hash ^ (value_hash + 0x9e3779b9 + (type_hash << 6) + (type_hash >> 2));
+      }, module);
+    }
+  };
+
+  template<>
+  struct hash<Modules> {
+    size_t operator()(const Modules& modules) const {
+      size_t seed = 0;
+      for (const auto& entry : modules) {
+        seed ^= std::hash<std::string>{}(entry.first) ^ (hash<Module>{}(entry.second) << 1);
+      }
+      return seed;
+    }
+  };
+
+  struct AlwaysDistinct {
+      bool operator()(const Modules& lhs, const Modules& rhs) const {
+          return false;
+      }
+  };  
 
   // Analyze and dig into the problem to find a good solution
   void explore(Model const& model) {
@@ -238,7 +290,6 @@ namespace part1 {
     inv -high-> a
 
     */
-   using Module = std::variant<detail::Module<decltype(broadcast)>,detail::Module<decltype(flip_flop)>,detail::Module<decltype(conjunction)>>;
     /*
 
     After this sequence, the flip-flop modules all end up off, so pushing the button again repeats the same sequence.
@@ -319,7 +370,71 @@ namespace part1 {
     waiting for all pulses to be fully handled after each push of the button. 
 
     What do you get if you multiply the total number of low pulses sent by the total number of high pulses sent?
-    */    
+    */
+    struct World {
+      Model model{};
+      Modules modules{};
+      Result m_low_count{ 0 };
+      Result m_high_count{ 0 };
+      std::unordered_set<Modules, hash<Modules>,AlwaysDistinct> seen{};
+      void act_on(std::string const& from, Signal pulse, std::string const& to) {
+        using Message = std::tuple<std::string,Signal,std::string>;
+        std::queue<Message> message_queue{};
+        message_queue.push({ from,pulse,to });
+        while (!seen.contains(modules) and !message_queue.empty()) {
+          auto [from,pulse,to] = message_queue.front();
+          message_queue.pop();
+          std::cout << NL << "act_on: " << std::quoted(from) << " " << (pulse ? (*pulse ? "high" : "low") : "null") << " " << std::quoted(to);
+          m_low_count += (pulse and *pulse == LOW);
+          m_high_count += (pulse and *pulse == HIGH);
+          std::cout << NL << "act_on: " << std::quoted(from) << " " << (pulse? "high" : "low") << " " << std::quoted(to);
+          auto it = std::find_if(model.begin(), model.end(), [&to](auto const& wiring) { return wiring.name == to; });
+          if (it != model.end()) {
+            auto [name,type,destinations] = *it;
+            if (modules.find(name) == modules.end()) {
+              switch (type[0]) {
+              case '%': modules.insert({name, detail::Module<decltype(flip_flop)>{}}); break;
+              case '&': modules.insert({name, detail::Module<decltype(conjunction)>{}}); break;
+              default: modules.insert({name, detail::Module<decltype(broadcast)>{}}); break;
+              }
+            }
+            
+            Signal out = std::visit(overloaded{
+              [from, pulse](detail::Module<decltype(flip_flop)>& module) {
+                return module.act_on(from, pulse);               
+              },
+              [from, pulse](detail::Module<decltype(conjunction)>& module) {
+                // handle conjunction module using 'from' and 'pulse'
+                return module.act_on(from, pulse);               
+              },
+              [from, pulse](detail::Module<decltype(broadcast)>& module) {
+                // handle broadcast module using 'from' and 'pulse'
+                return module.act_on(from, pulse);               
+              }
+            }, modules.at(name));
+            for (auto const& destination : destinations) {
+              message_queue.push({ name,out,destination });
+            }
+          }
+          else std::cout << NL << "failed to find: " << std::quoted(to);
+        }
+
+      }
+      Result low_count() {
+        return m_low_count;
+      }
+      Result high_count() {
+        return m_high_count;
+      }
+    };
+    auto world = World{model};
+    for (int i = 0; i < 1000; ++i) {
+      world.act_on("button", LOW,"broadcaster");
+    };
+    auto low_count = world.low_count();
+    auto high_count = world.high_count();
+    Result result = low_count * high_count;
+    std::cout << NL << NL << "==> explore says result : " << result; 
 
   }
   Result solve_for(Model& model) {
