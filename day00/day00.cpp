11ea4f2bdc3670bd7dfe2e0cac872feb10609abd
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-21 21:50:52 +0100

    day00 template update to pass CLI arguments to dynamic part and in stream

--- day00/day00.cpp
+++ day00/day00.cpp
@@ -33,10 +33,7 @@ using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64
 using Result = Integer;
 using Answer = std::pair<std::string, Result>;
 using Answers = std::vector<Answer>;
-struct Solution {
-  Answers part1{};
-  Answers part2{};
-};
+using Solution = std::map<int, Answers>; // Puzzle part -> Answers
 
 using Model = std::vector<std::string>;
 
@@ -50,15 +47,17 @@ Model parse(auto& in) {
 }
 
 namespace part1 {
-  Result solve_for(Model& model) {
+  Result solve_for(Model& model,auto args) {
     Result result{};
+    std::cout << NL << NL << "part1";
     return result;
   }
 }
 
 namespace part2 {
-  Result solve_for(Model& model) {
+  Result solve_for(Model& model,auto args) {
     Result result{};
+    std::cout << NL << NL << "part2";
     return result;
   }
 }
@@ -67,45 +66,43 @@ int main(int argc, char *argv[])
 {
   Solution solution{};
   std::cout << NL << "argc : " << argc;
-  Answer part1_answer{ "Failed to obtain any input",0 };
-  Answer part2_answer{ "Failed to obtain any input",0 };
-  if (argc == 1) {
-    std::cout << NL << "no data file provided ==> WIll use hard coded example input";
-    std::istringstream in{ example };
-    auto model = parse(in);
-    part1_answer = { "Example",part1::solve_for(model) };
-    part2_answer = { "Example",part2::solve_for(model) };
-    solution.part1.push_back(part1_answer);
-    solution.part2.push_back(part2_answer);
-  }
-  else for (int i = 0; i < argc; ++i) {
+  for (int i = 0; i < argc; ++i) {
     std::cout << NL << "argv[" << i << "] : " << std::quoted(argv[i]);
-    if (i > 0) {
-      std::ifstream in{ argv[i] };
-      if (in) {
-        auto model = parse(in);
-        part1_answer = { argv[i],part1::solve_for(model) };
-        part2_answer = { argv[i],part2::solve_for(model) };
-      }
-      else {
-        part1_answer = { std::string{"Failed to open file "} + argv[i],-1 };
-        part2_answer = { std::string{"Failed to open file "} + argv[i],-1 };
-      }
-      solution.part1.push_back(part1_answer);
-      solution.part2.push_back(part2_answer);
+  }
+  // day00 x y
+  std::tuple<int,std::string> args{1,"example.txt"};
+  auto& [part,file] = args;
+  if (argc > 1 ) {
+    part = std::stoi(argv[1]);
+    if (argc > 2) {
+      file = argv[2];
     }
   }
+  std::cout << NL << "Part : " << part << " file : " << file;
+  std::ifstream in{ file };
+  auto model = parse(in);
+
+  switch (part) {
+  case 1: {
+    auto answer = part1::solve_for(model,args);
+    solution[part].push_back({ file,answer });
+  } break;
+  case 2: {
+    auto answer = part2::solve_for(model,args);
+    solution[part].push_back({ file,answer });
+  } break;
+  default:
+    std::cout << NL << "No part " << part << " only part 1 and 2";
+  }
+
   std::cout << NL << NL << "------------ REPORT----------------";
-  std::cout << NL << "<Part 1>";
-  for (auto const& answer : solution.part1) {
-    if (answer.second != 0) std::cout << NT << "answer[" << answer.first << "] " << answer.second;
-    else std::cout << NT << "answer[" << answer.first << "] " << " NO OPERATION ";
+  for (auto const& [part, answers] : solution) {
+    std::cout << NL << "Part " << part << " answers";
+    for (auto const& [heading, answer] : answers) {
+      if (answer != 0) std::cout << NT << "answer[" << heading << "] " << answer;
+      else std::cout << NT << "answer[" << heading << "] " << " NO OPERATION ";
+    }
   }
-  std::cout << NL << "<Part 2>";
-  for (auto const& answer : solution.part2) {
-    if (answer.second != 0) std::cout << NT << "answer[" << answer.first << "] " << answer.second;
-    else std::cout << NT << "answer[" << answer.first << "] " << " NO OPERATION ";
-  }
-  std::cout << "\n\n";
+  std::cout << NL << NL;
   return 0;
 }
