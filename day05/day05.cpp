0ec99dd915bc393aca51827cbd6ec173b8810cd7
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-21 17:31:42 +0100

    day05 part 2 example ok (using hyperneutrino python code refactored to C++)

--- day05/day05.cpp
+++ day05/day05.cpp
@@ -17,12 +17,145 @@
 #include <fstream>
 #include <cstring>
 
+
 auto const NL = "\n";
+auto const T = "\t";
 auto const NT = "\n\t";
 
 using Integer = long long; // int: 843 253 387 long int: 32 762 853 787 275 long long int: 32 762 853 787 275
 using Result = Integer;
-using Answers = std::vector<std::pair<std::string,Result>>;
+using Answer = std::pair<std::string, Result>;
+using Answers = std::vector<Answer>;
+using Solution = std::map<int, Answers>; // Puzzle part -> Answers
+
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
+using Result = Integer;
+using Answer = std::pair<std::string, Result>;
+using Answers = std::vector<Answer>;
+using Solution = std::map<int, Answers>; // Puzzle part -> Answers
+
+/*
+--- Day 5: If You Give A Seed A Fertilizer ---
+
+You take the boat and find the gardener right where you were told he would be: managing a giant "garden" that looks more to you like a farm.
+
+"A water source? Island Island is the water source!" You point out that Snow Island isn't receiving any water.
+
+"Oh, we had to stop the water because we ran out of sand to filter it with! Can't make snow with dirty water. Don't worry, I'm sure we'll get more sand soon; we only turned off the water a few days... weeks... oh no." His face sinks into a look of horrified realization.
+
+"I've been so busy making sure everyone here has food that I completely forgot to check why we stopped getting more sand! There's a ferry leaving soon that is headed over in that direction - it's much faster than your boat. Could you please go check it out?"
+
+You barely have time to agree to this request when he brings up another. "While you wait for the ferry, maybe you can help us with our food production problem. The latest Island Island Almanac just arrived and we're having trouble making sense of it."
+
+The almanac (your puzzle input) lists all of the seeds that need to be planted. It also lists what type of soil to use with each kind of seed, what type of fertilizer to use with each kind of soil, what type of water to use with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer and so on is identified with a number, but numbers are reused by each category - that is, soil 123 and fertilizer 123 aren't necessarily related to each other.
+
+For example:
+
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+The almanac starts by listing which seeds need to be planted: seeds 79, 14, 55, and 13.
+
+The rest of the almanac contains a list of maps which describe how to convert numbers from a source category into numbers in a destination category. That is, the section that starts with seed-to-soil map: describes how to convert a seed number (the source) to a soil number (the destination). This lets the gardener and his team know which soil to use with which seeds, which water to use with which fertilizer, and so on.
+
+Rather than list every source number and its corresponding destination number one by one, the maps describe entire ranges of numbers that can be converted. Each line within a map contains three numbers: the destination range start, the source range start, and the range length.
+
+Consider again the example seed-to-soil map:
+
+50 98 2
+52 50 48
+The first line has a destination range start of 50, a source range start of 98, and a range length of 2. This line means that the source range starts at 98 and contains two values: 98 and 99. The destination range is the same length, but it starts at 50, so its two values are 50 and 51. With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
+
+The second line means that the source range starts at 50 and contains 48 values: 50, 51, ..., 96, 97. This corresponds to a destination range starting at 52 and also containing 48 values: 52, 53, ..., 98, 99. So, seed number 53 corresponds to soil number 55.
+
+Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10.
+
+So, the entire list of seed numbers and their corresponding soil numbers looks like this:
+
+seed  soil
+0     0
+1     1
+...   ...
+48    48
+49    49
+50    52
+51    53
+...   ...
+96    98
+97    99
+98    50
+99    51
+With this map, you can look up the soil number required for each initial seed number:
+
+Seed number 79 corresponds to soil number 81.
+Seed number 14 corresponds to soil number 14.
+Seed number 55 corresponds to soil number 57.
+Seed number 13 corresponds to soil number 13.
+The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
+
+Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+So, the lowest location number in this example is 35.
+
+What is the lowest location number that corresponds to any of the initial seed numbers?
+
+*/
+
+/*
+
+--- Part Two ---
+
+Everyone will starve if you only plant such a small number of seeds. Re-reading the almanac, it looks like the seeds: line actually describes ranges of seed numbers.
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+
+Answer:  
+
+Although it hasn't changed, you can still get your puzzle input.
+
+You can also [Share] this puzzle.
+*/
 
 enum Category {
   undefined
@@ -221,13 +354,117 @@ namespace part1 {
           acc = std::min(candidate.value,acc);
           return acc;});
       }
-      return result;
+      return result;// 57075758
   }
 }
 
 namespace part2 {
+  namespace hyperneutrino {
+
+    int main(auto& file) {
+      std::string line{};
+      std::getline(file,line);
+      std::cout << NL << "parsing:" << std::quoted(line) << std::flush;
+      std::pair<Category,Category> mapping{undefined,undefined};
+      std::vector<int> input_values;
+      auto s = line.substr(std::strlen("seeds:")+1);
+      std::cout << " as seeds " << std::quoted(s) << "-->";
+      std::istringstream ss{s};
+      std::string n;
+      while (ss >> n) {
+        std::cout << " value:" << n << std::flush;
+        input_values.push_back(std::stoll(n));
+      }
+
+
+      // std::getline(file, line);
+      // std::string_view inputs = line.substr(line.find(':') + 1);
+      // std::cout << NL << "entry:" << std::quoted(line) << std::flush;
+      // std::vector<int> input_values;
+      // auto temp = inputs;
+      // while (auto pos = inputs.find(' '); pos != std::string_view::npos) {
+      //   temp = inputs.substr(0, pos);
+      //   std::cout << NT << "at ':' " << std::quoted(temp) << std::flush;
+      //   input_values.push_back(std::stoi(temp));
+      //   temp.remove_prefix(pos + 1);
+      // }
+      // input_values.push_back(std::stoi(temp));
+
+      std::vector<std::pair<int, int>> seeds;
+      for (int i = 0; i < input_values.size(); i += 2) {
+        seeds.push_back({input_values[i], input_values[i] + input_values[i + 1]});
+      }
+
+      while (file) {
+        std::string block;
+        while (std::getline(file, line) and !line.empty()) block += line + '\n';
+        std::cout << NL << "block:" << std::quoted(block) << std::flush;
+
+        std::vector<std::vector<int>> ranges;
+        std::istringstream iss(block);
+        std::string line;
+        while (std::getline(iss, line)) {
+          std::cout << NL << NT << "line:" << std::quoted(line) << std::flush;
+          if (line.find(':') != std::string::npos) continue; // skip header
+          std::istringstream iss2(line);
+          std::vector<int> temp;
+          int value;
+          while (iss2 >> value) {
+            std::cout << NT << NT << "value:" << value << std::flush;
+            temp.push_back(value);
+          }
+          std::cout << NT << "push temp size:" << temp.size() << std::flush;
+          ranges.push_back(temp);
+        }
+
+        std::cout << NL << "while seeds : size:" << seeds.size() << std::flush;
+
+        std::vector<std::pair<int, int>> new_seeds;
+        while (!seeds.empty()) {
+          auto [s, e] = seeds.back();
+          std::cout << NT << "seed:" << s << ".." << e << std::flush;
+          seeds.pop_back();
+          bool broken = false;
+          for (auto& range : ranges) {
+            std::cout << NT << "range size:" << range.size() << std::flush;
+            int a = range[0], b = range[1], c = range[2];
+            int os = std::max(s, b);
+            int oe = std::min(e, b + c);
+            if (os < oe) {
+              std::cout << NT << NT << "new seeds " << os - b + a << ".." << oe - b + a << std::flush;
+              new_seeds.push_back({os - b + a, oe - b + a});
+              if (os > s) {
+                seeds.push_back({s, os});
+              }
+              if (e > oe) {
+                seeds.push_back({oe, e});
+              }
+              broken=true;
+              break;
+            }
+          }
+          if (!broken) {
+            std::cout << NT << NT << "new seeds" << s << ".." << e << std::flush;
+            new_seeds.push_back({s, e});
+          }
+        }
+        seeds = new_seeds;
+      }
+
+      std::cout << NL << "seeds size:" << seeds.size() << std::flush;
+      for (auto [s, e] : seeds) std::cout << NT << "seed:" << s << ".." << e << std::flush;
+
+      Result result = std::min_element(seeds.begin(), seeds.end())->first; 
+
+      std::cout << NL << "hyperneutrino says: " << result << std::endl;
+
+      return result;
+    }
+  } // namespace hyperneutrino
   Result solve_for(auto& in) {
       Result result{std::numeric_limits<Result>::max()};
+      if (true) return hyperneutrino::main(in);
+
       auto model = parse(in);
       for (auto i=0;i<model.seeds.size();i+=2) {
         for (auto j=model.seeds[i];j<model.seeds[i+1];++j) {
@@ -246,22 +483,44 @@ namespace part2 {
 
 int main(int argc, char *argv[])
 {
-  Answers answers{};
+  Solution solution{};
   std::cout << NL << "argc : " << argc;
-  for (int i=0;i<argc;++i) {
+  for (int i = 0; i < argc; ++i) {
     std::cout << NL << "argv[" << i << "] : " << std::quoted(argv[i]);
-    if (i>0) {
-      std::ifstream in{argv[i]};
-      // if (in) answers.push_back({argv[i],part1::solve_for(in)}); // 57075758
-      if (in) answers.push_back({argv[i],part2::solve_for(in)});
-      else answers.push_back({std::string{"Failed to open file "} + argv[i],-1});
+  }
+  // day05 part file
+  int part{1};
+  std::string file{"example.txt"};
+  if (argc > 1 ) {
+    part = std::stoi(argv[1]);
+    if (argc > 2) {
+      file = argv[2];
     }
   }
-  std::cout << "\n\n------------ REPORT----------------";
-  for (auto const& answer : answers) {
-    if (answer.second!=0) std::cout << "\nanswer[" << answer.first << "] " << answer.second;
-    else std::cout << "\nanswer[" << answer.first << "] " << " NO OPERATION ";
+  std::cout << NL << "Part : " << part << " file : " << file;
+  std::ifstream in{ file };
+
+  switch (part) {
+  case 1: {
+    auto answer = part1::solve_for(in);
+    solution[part].push_back({ file,answer });
+  } break;
+  case 2: {
+    auto answer = part2::solve_for(in);
+    solution[part].push_back({ file,answer });
+  } break;
+  default:
+    std::cout << NL << "No part " << part << " only part 1 and 2";
   }
-  std::cout << "\n\n";
+
+  std::cout << NL << NL << "------------ REPORT----------------";
+  for (auto const& [part, answers] : solution) {
+    std::cout << NL << "Part " << part << " answers";
+    for (auto const& [heading, answer] : answers) {
+      if (answer != 0) std::cout << NT << "answer[" << heading << "] " << answer;
+      else std::cout << NT << "answer[" << heading << "] " << " NO OPERATION ";
+    }
+  }
+  std::cout << NL << NL;
   return 0;
 }
