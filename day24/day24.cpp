ba27cf885a74e269f514dcc130f1ce09cd743d41
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-25 00:13:25 +0100

    day24 part 2 example ok (using refactored code by tbeu with a reference-to-deleted-stack-frame bug fixed)

--- day24/day24.cpp
+++ day24/day24.cpp
@@ -146,6 +146,74 @@ How many of these intersections occur within the test area?
 
 */
 
+/*
+--- Part Two ---
+
+Upon further analysis, it doesn't seem like any hailstones will naturally collide. 
+It's up to you to fix that!
+
+You find a rock on the ground nearby. 
+While it seems extremely unlikely, 
+if you throw it just right, 
+you should be able to hit every hailstone in a single throw!
+
+You can use the probably-magical winds to reach any integer position you like 
+and to propel the rock at any integer velocity. 
+Now including the Z axis in your calculations, 
+if you throw the rock at time 0, 
+where do you need to be 
+  so that the rock perfectly collides with every hailstone? 
+  Due to probably-magical inertia, 
+  the rock won't slow down or change direction when it collides with a hailstone.
+
+In the example above, you can achieve this 
+by moving to position 24, 13, 10 
+and throwing the rock at velocity -3, 1, 2. 
+
+If you do this, you will hit every hailstone as follows:
+
+Hailstone: 19, 13, 30 @ -2, 1, -2
+Collision time: 5
+Collision position: 9, 18, 20
+
+Hailstone: 18, 19, 22 @ -1, -1, -2
+Collision time: 3
+Collision position: 15, 16, 16
+
+Hailstone: 20, 25, 34 @ -2, -2, -4
+Collision time: 4
+Collision position: 12, 17, 18
+
+Hailstone: 12, 31, 28 @ -1, -2, -1
+Collision time: 6
+Collision position: 6, 19, 22
+
+Hailstone: 20, 19, 15 @ 1, -5, -3
+Collision time: 1
+Collision position: 21, 14, 12
+
+Above, each hailstone is identified 
+by its initial position and its velocity. 
+Then, the time and position of that hailstone's collision with your rock are given.
+
+After 1 nanosecond, 
+the rock has exactly the same position as one of the hailstones, 
+obliterating it into ice dust! 
+Another hailstone is smashed to bits two nanoseconds after that. 
+
+After a total of 6 nanoseconds, all of the hailstones have been destroyed.
+
+So, at time 0, the rock needs to be at X position 24, Y position 13, and Z position 10. 
+
+Adding these three coordinates together produces 47. (Don't add any coordinates from the rock's velocity.)
+
+Determine the exact position and velocity the rock needs to have at time 0 
+so that it perfectly collides with every hailstone. 
+
+What do you get if you add up the X, Y, and Z coordinates of that initial position?
+
+*/
+
 class Parser {
 public:
   using Candidate = std::pair<std::string_view, std::string_view>;
@@ -211,24 +279,24 @@ void print_model(Model const& model) {
   }
 }
 
+// Hailstone trajectory on the form ax + by  = c
+// with: a = dx/dt, b = -dy/dt, the velocities vx and vy.
+class Hailstone {
+public:
+  Integer sx, sy, sz, vx, vy, vz;
+  Integer a, b, c;
+
+  Hailstone(Integer sx, Integer sy, Integer sz, Integer vx, Integer vy, Integer vz)
+    : sx(sx), sy(sy), sz(sz), vx(vx), vy(vy), vz(vz), a(vy), b(-vx), c(vy * sx - vx * sy) {}
+
+  friend std::ostream& operator<<(std::ostream& os, const Hailstone& hs) {
+    return os << "Hailstone{a=" << hs.a << ", b=" << hs.b << ", c=" << hs.c << "}";
+  }
+};
+
 namespace part1 {
-  // refactored from hyperneutrino youtube video 
+  // Refactored from hyperneutrino python https://github.com/hyper-neutrino/advent-of-code/blob/main/2023/day24p1.py
   namespace hyperneutrino {
-
-    // Hailstone trajectory on the form ax + by  = c
-    // with: a = dx/dt, b = -dy/dt, the velocities vx and vy.
-    class Hailstone {
-    public:
-      Integer sx, sy, sz, vx, vy, vz;
-      Integer a, b, c;
-
-      Hailstone(Integer sx, Integer sy, Integer sz, Integer vx, Integer vy, Integer vz)
-        : sx(sx), sy(sy), sz(sz), vx(vx), vy(vy), vz(vz), a(vy), b(-vx), c(vy * sx - vx * sy) {}
-
-      friend std::ostream& operator<<(std::ostream& os, const Hailstone& hs) {
-        return os << "Hailstone{a=" << hs.a << ", b=" << hs.b << ", c=" << hs.c << "}";
-      }
-    };
 
     int count(Model const& model,auto args) {
       std::vector<Hailstone> hailstones;
@@ -246,15 +314,16 @@ namespace part1 {
           // e2: a2 * x + b2 * y = c2
           // e1 * a2 - e2 * a1: (a1 * b2 - a2 * b1) * y = c1 * a2 - c2 * a1; x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
           // e2 * b1 - e1 * b2: (a1 * b2 - a2 * b1) * x = c2 * b1 - c1 * b2; y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
-          // BUT: a1 * b2 - a2 * b1 == 0 iff a1 * b2 == a2 * b1 (the lines are parallel = never intersects)
+          // BUT: a1 * b2 - a2 * b1 == 0 when a1 * b2 == a2 * b1 (the lines are parallel = never intersects)
           double a1 = hailstones[i].a, b1 = hailstones[i].b, c1 = hailstones[i].c;
           double a2 = hailstones[j].a, b2 = hailstones[j].b, c2 = hailstones[j].c;
           if (a1 * b2 == b1 * a2) {
             std::cout << NL << "Parallel lines i:" << i << " j:" << j;
             continue; // skip parallel lines
           }
-          auto x = static_cast<double>(c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
-          auto y = static_cast<double>(c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1);
+          // Note: To ensure the multiplication does not overflow we ensure the arguments are floats and not int64_t (See a and b arguments above)
+          auto x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+          auto y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1);
           std::cout << NL << "Intersection candidate x : " << x << " y : " << y;
           if (min <= x && x <= max && min <= y && y <= max) {
             // Intersection point is in the test area
@@ -296,9 +365,235 @@ namespace part1 {
 }
 
 namespace part2 {
+  // Refactored from hyperneutrino python https://github.com/hyper-neutrino/advent-of-code/blob/main/2023/day24p2.py
+  // Note: This python code uses a python library to solve the system of equations.
+  // The C++ code below solves the system of equations directly.
+  namespace hyperneutrino {
+
+    class Hailstone {
+    public:
+      Integer sx, sy, sz, vx, vy, vz;
+      Integer a, b, c, d;
+
+        // From:
+        // x = x0 + vx * t
+        // y = y0 + vy * t
+        // z = z0 + vz * t
+        // eliminate t
+        // x(vy-vz) + y(vz-vx) + z(vx-vy) = x0(vy-vz) + y0(vz-vx) + z0(vx-vy)
+        // x(vy-vz) + y(vz-vx) + z(vx-vy) - x0(vy-vz) - y0(vz-vx) - z0(vx-vy) = 0
+        // ax + by + cz = d
+        // with a = vy-vz, b = vz-vx, c = vx-vy, d = x0(vy-vz) + y0(vz-vx) + z0(vx-vy)
+      Hailstone(Integer sx, Integer sy, Integer sz, Integer vx, Integer vy, Integer vz)
+        : sx(sx), sy(sy), sz(sz), vx(vx), vy(vy), vz(vz), a(vy-vz), b(vz-vx), c(vx-vy),d(sx*(vy-vz) + sy*(vz-vx) + sz*(vx-vy)) {}
+
+      friend std::ostream& operator<<(std::ostream& os, const Hailstone& hs) {
+        return os << "Hailstone{a=" << hs.a << ", b=" << hs.b << ", c=" << hs.c << "}";
+      }
+    };
+
+    using Equation = std::array<Integer,4>; // a,b,c,d in ax + by + cz - d = 0
+    using System = std::vector<Equation>;
+
+    std::optional<System> solve(System system) {
+      // Solve the system of equations using Gauss-Jordan elimination
+      if (std::all_of(system.begin(), system.end(), [](auto const& eq) { return std::count(eq.begin(), eq.end(), 0) == eq.size()-1;})) {
+        // All equations are 0 = 0
+        return system;
+      }
+      else return std::nullopt; // System is underdetermined
+    }
+
+    int count(Model const& model,auto args) {
+      std::vector<Hailstone> hailstones;
+      for (auto const& entry : model) {
+        hailstones.push_back({entry[0], entry[1], entry[2], entry[3], entry[4], entry[5]});
+      }
+
+      int total = 0;
+      auto const& [_,__,min,max] = args;
+
+      for (Integer i = 0; i < hailstones.size(); ++i) {
+        for (Integer j = 0; j < i; ++j) {
+          // We are looking for the trajectory of a rock with which all hailstones will intersect.
+          // As the only thing we can control is the thrown rock meaning we have siz degrees of freedom.
+          // We need to solve for the six unknowns sx,sy,sz,vx,vy,vz.
+          // So worst case we need six independent equations to solve for six unknowns.
+          // Now, how many hailstone trajectories do we need to tie down all the six unknowns of the thrown stone trajectory?
+          // Well, we dont know. It depends on how the hailstone trajectories we seek to intersect with relate to each other.
+
+          // Wait! No, for our thrown stone and a hailstone to actually collide, they need to be at the same location at the same time!
+          // So, for each stone position p(t) = p0 + v * t, and hailstone position h(t) = h0 + u * t, there need to be a t when p(t) = h(t).
+          // But the stone does not have to hit all the hailstones at the same time, it can hit one at a time.
+          // Also, we are not actually interested in the time of collision, only the trajectory of the thrown stone.
+          // hm...
+        }
+      }
+
+      std::cout << NL << "hyperneutrion says : " << total << std::endl;
+
+      return total;
+    }
+  }
+
+  // Refactored from C++ solution https://github.com/tbeu/AdventOfCode/blob/master/2023/day24/day24.cpp
+  // many thanks!
+  namespace tbeu {
+
+    using V = std::array<int64_t, 3>;
+    using Line = std::array<V, 2>;
+    using Lines = std::vector<Line>;
+
+    V operator-(const V& lhs, const V& rhs) {
+      return {lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]};
+    }
+
+    Line operator-(const Line& lhs, const Line& rhs) {
+      Line result;
+      result[0] = lhs[0] - rhs[0];
+      result[1] = lhs[1] - rhs[1];
+      return result;
+    }
+
+    // Glu my model with tbeu Lines
+    Lines to_lines(Model const& model) {
+      Lines lines;
+      for (auto const& entry : model) {
+        V v1{entry[0], entry[1], entry[2]};
+        V v2{entry[3], entry[4], entry[5]};
+        Line line{v1,v2};
+        lines.push_back(line);
+      }
+      return lines;
+    }
+
+    template <typename INTERSECT_TYPE>
+    static std::optional<std::array<INTERSECT_TYPE, 2>>
+    intersect(const Line &a, const Line &b) {
+        const auto &p1 = a[0];
+        const auto &v1 = a[1];
+        const auto &p2 = b[0];
+        const auto &v2 = b[1];
+        INTERSECT_TYPE s, t;
+        const auto den1 = v2[0] * v1[1];
+        const auto den2 = v2[1] * v1[0];
+        if (den1 == den2) {
+            // parallel or identical
+            return std::nullopt;
+        } else {
+            const auto nom1 =
+                ((p2[1] - p1[1]) * v1[0] + (p1[0] - p2[0]) * v1[1]);
+            if constexpr (std::is_integral_v<INTERSECT_TYPE>) {
+                if (0 != nom1 % (den1 - den2)) {
+                    // not an integer solution
+                    return std::nullopt;
+                }
+            }
+            s = nom1 / static_cast<INTERSECT_TYPE>(den1 - den2);
+            const auto nom2 = (p2[0] - p1[0]) + s * v2[0];
+            t = nom2 / static_cast<INTERSECT_TYPE>(v1[0]);
+        }
+        return std::array<INTERSECT_TYPE, 2>{t, s};
+    }
+    template <typename INTERSECT_TYPE>
+    static bool checkIntersect(const Line &a, const Line &b, int64_t start,
+                               int64_t end, bool ignoreZ) {
+        const auto ts = intersect<INTERSECT_TYPE>(a, b);
+        const auto &p1 = a[0];
+        const auto &v1 = a[1];
+        const auto &p2 = b[0];
+        const auto &v2 = b[1];
+        if (!ts.has_value()) {
+            const auto isParallel =
+                (p2[0] - p1[0]) * v1[1] == (p2[1] - p1[1]) * v1[0];
+            if (ignoreZ) {
+                return isParallel;
+            } else {
+                return isParallel ||
+                       (p2[0] - p1[0]) * v1[2] == (p2[2] - p1[2]) * v1[0];
+            }
+        }
+        const auto [t, s] = ts.value();
+        auto isInter = s >= 0 && t >= 0;
+        if constexpr (std::is_integral_v<INTERSECT_TYPE>) {
+            if (isInter && !ignoreZ) {
+                const auto r1 = p1[2] + t * v1[2];
+                const auto r2 = p2[2] + s * v2[2];
+                isInter = isInter && r1 == r2;
+            }
+        } else {
+            for (uint8_t i = 0; i < 3; ++i) {
+                if (!isInter) {
+                    break;
+                }
+                if (ignoreZ && 2 == i) {
+                    break;
+                }
+                const auto r1 = p1[i] + t * v1[i];
+                const auto r2 = p2[i] + s * v2[i];
+                isInter = isInter && r1 >= start && r2 >= start;
+                isInter = isInter && r1 <= end && r2 <= end;
+            }
+        }
+        return isInter;
+    }
+
+    template <typename INTERSECT_TYPE>
+    static bool checkIntersect(const Lines &lines, const V &vDiff, bool ignoreZ = true)
+    {
+      for (size_t i = 0; i < lines.size(); ++i)
+      {
+        for (size_t j = i + 1; j < lines.size(); ++j)
+        {
+          const auto &pi = lines[i][0];
+          const auto &vi = lines[i][1] - vDiff;
+          const auto &pj = lines[j][0];
+          const auto &vj = lines[j][1] - vDiff;
+          if (!checkIntersect<INTERSECT_TYPE>({pi, vi}, {pj, vj}, INT64_MIN, INT64_MAX, ignoreZ))
+          {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    static V findRock(const Lines &lines)
+    {
+      constexpr int64_t vRange = 300;
+      for (int64_t v1 = -vRange; v1 <= vRange; ++v1)
+      {
+        for (int64_t v2 = -vRange; v2 <= vRange; ++v2)
+        {
+          if (const auto v = V{v1, v2, 0}; !checkIntersect<int64_t>(lines, v, true))
+          {
+            continue;
+          }
+          for (int64_t v3 = -vRange; v3 <= vRange; ++v3)
+          {
+            if (const auto v = V{v1, v2, v3}; checkIntersect<int64_t>(lines, v, false))
+            {
+              // get the actual intersection point (from any two lines)
+              const auto pi = lines[0][0];
+              const auto vi = lines[0][1] - v;
+              const auto pj = lines[1][0];
+              const auto vj = lines[1][1] - v;
+              const auto [t, s] = intersect<int64_t>({pi, vi}, {pj, vj}).value();
+              // std::cout << NL << "tbeu says: " << " " <<  pi[0] + t * vi[0] << " , " <<  pi[1] + t * vi[1] << " , " <<  pi[2] + t * vi[2];
+              return V{pi[0] + t * vi[0], pi[1] + t * vi[1], pi[2] + t * vi[2]};
+            }
+          }
+        }
+      }
+      return V{0, 0, 0};
+    }
+  }
+
   Result solve_for(Model& model,auto args) {
     Result result{};
     std::cout << NL << NL << "part2";
+    const auto rock = tbeu::findRock(tbeu::to_lines(model));
+    result = rock[0] + rock[1] + rock[2];
     return result;
   }
 }
