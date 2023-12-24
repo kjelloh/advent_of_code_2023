f40e346d111b07b2280ae69692887758e4984df3
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2023-12-24 12:41:19 +0100

    day24 part 1 example - parser using simple parser class SplitOn to model of 6-valued arrays and model print ok.

---
+++ day24/day24.cpp
@@ -0,0 +1,273 @@
+#include <cctype>
+#include <iostream>
+#include <iomanip> // E.g., std::quoted
+#include <string>
+#include <string_view>
+#include <sstream> // E.g., std::istringstream, std::ostringstream
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <ranges> // E.g., std::subrange, std::view 
+#include <utility> // E.g., std::pair,..
+#include <algorithm> // E.g., std::find, std::all_of,...
+#include <numeric> // E.g., std::accumulate
+#include <limits> // E.g., std::numeric_limits
+#include <fstream>
+// #include <experimental/generator> // supported by visual studio 2022 17.8.2 with project setting/compiler switch /std:c++latest
+#include <format>
+#include <optional>
+#include <regex>
+
+
+char const* example = R"(19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
+)";
+std::tuple<int,int> const example_test_area{7,27}; // x,y
+
+auto const NL = "\n";
+auto const T = "\t";
+auto const NT = "\n\t";
+
+using Integer = int64_t; // 16 bit int: 3.27 x 10^4, 32 bit int: 2.14 x 10^9, 64 bit int: 9.22 x 10^18
+using Result = Integer;
+using Answer = std::pair<std::string, Result>;
+using Answers = std::vector<Answer>;
+using Solution = std::map<int, Answers>; // Puzzle part -> Answers
+
+/*
+--- Day 24: Never Tell Me The Odds ---
+
+It seems like something is going wrong with the snow-making process. 
+Instead of forming snow, the water that's been absorbed into the air seems to be forming hail!
+
+Maybe there's something you can do to break up the hailstones?
+
+Due to strong, probably-magical winds, 
+the hailstones are all flying through the air in perfectly linear trajectories. 
+You make a note of each hailstone's position and velocity (your puzzle input). 
+
+For example:
+
+19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
+
+Each line of text corresponds to the position and velocity of a single hailstone. 
+The positions indicate where the hailstones are right now (at time 0). 
+The velocities are constant and indicate exactly how far each hailstone will move in one nanosecond.
+
+Each line of text uses the format px py pz @ vx vy vz. 
+For instance, the hailstone specified by 20, 19, 15 @ 1, -5, -3 
+  has initial X position 20, Y position 19, Z position 15, 
+  X velocity 1, Y velocity -5, and Z velocity -3. 
+  After one nanosecond, the hailstone would be at 21, 14, 12.
+
+Perhaps you won't have to do anything. 
+How likely are the hailstones to collide with each other and smash into tiny ice crystals?
+
+To estimate this, 
+consider only the X and Y axes; 
+  ==> ignore the Z axis. 
+  
+Looking forward in time, 
+how many of the hailstones' paths will intersect within a test area? 
+  (The hailstones themselves don't have to collide, 
+   just test for intersections between the paths they will trace.)
+
+In this example, 
+look for intersections that happen with an X and Y position each at least 7 and at most 27; 
+in your actual data, you'll need to check a much larger test area. 
+Comparing all pairs of hailstones' future paths produces the following results:
+
+Hailstone A: 19, 13, 30 @ -2, 1, -2
+Hailstone B: 18, 19, 22 @ -1, -1, -2
+Hailstones' paths will cross inside the test area (at x=14.333, y=15.333).
+
+Hailstone A: 19, 13, 30 @ -2, 1, -2
+Hailstone B: 20, 25, 34 @ -2, -2, -4
+Hailstones' paths will cross inside the test area (at x=11.667, y=16.667).
+
+Hailstone A: 19, 13, 30 @ -2, 1, -2
+Hailstone B: 12, 31, 28 @ -1, -2, -1
+Hailstones' paths will cross outside the test area (at x=6.2, y=19.4).
+
+Hailstone A: 19, 13, 30 @ -2, 1, -2
+Hailstone B: 20, 19, 15 @ 1, -5, -3
+Hailstones' paths crossed in the past for hailstone A.
+
+Hailstone A: 18, 19, 22 @ -1, -1, -2
+Hailstone B: 20, 25, 34 @ -2, -2, -4
+Hailstones' paths are parallel; they never intersect.
+
+Hailstone A: 18, 19, 22 @ -1, -1, -2
+Hailstone B: 12, 31, 28 @ -1, -2, -1
+Hailstones' paths will cross outside the test area (at x=-6, y=-5).
+
+Hailstone A: 18, 19, 22 @ -1, -1, -2
+Hailstone B: 20, 19, 15 @ 1, -5, -3
+Hailstones' paths crossed in the past for both hailstones.
+
+Hailstone A: 20, 25, 34 @ -2, -2, -4
+Hailstone B: 12, 31, 28 @ -1, -2, -1
+Hailstones' paths will cross outside the test area (at x=-2, y=3).
+
+Hailstone A: 20, 25, 34 @ -2, -2, -4
+Hailstone B: 20, 19, 15 @ 1, -5, -3
+Hailstones' paths crossed in the past for hailstone B.
+
+Hailstone A: 12, 31, 28 @ -1, -2, -1
+Hailstone B: 20, 19, 15 @ 1, -5, -3
+Hailstones' paths crossed in the past for both hailstones.
+
+So, in this example, 
+2 hailstones' future paths cross inside the boundaries of the test area.
+
+However, you'll need to search a much larger test area 
+if you want to see if any hailstones might collide. 
+
+Look for intersections that happen 
+  with an X and Y position each at least 200000000000000 and at most 400000000000000. 
+  Disregard the Z axis entirely.
+
+Considering only the X and Y axes, 
+check all pairs of hailstones' future paths for intersections. 
+
+How many of these intersections occur within the test area?
+
+*/
+
+class Parser {
+public:
+  using Candidate = std::pair<std::string_view, std::string_view>;
+  using Candidates = std::vector<Candidate>;
+    virtual Candidates parse(std::string_view input) = 0;
+};
+
+template <char C>
+class SplitOn : public Parser  {
+public:
+  using Candidate = std::pair<std::string_view, std::string_view>;
+  using Candidates = std::vector<Candidate>;
+    Candidates parse(std::string_view input) {
+      if (input.empty()) return Candidates{}; // return empty parse
+      Candidates result;
+      // Find the next char C
+      size_t pos = input.find(C);
+
+      // If there's no char C, this is the tail
+      if (pos == std::string_view::npos)
+      {
+        result.push_back({input,{}});
+      }
+      else {
+        auto head = input.substr(0, pos);
+        auto tail = input.substr(pos + 1);
+        result.push_back({head,tail});
+      }
+      return result;
+    }
+};
+
+using Entry = std::array<int,6>; // px py pz @ vx vy vz
+using Model = std::vector<Entry>;
+
+Model parse(auto& in) {
+  Model result{};
+  std::string line{};
+  while (std::getline(in,line)) {
+    result.push_back({});
+    auto split_line = SplitOn<'@'>{}.parse(line)[0]; // assume success
+    std::cout << NT << "head : " << std::quoted(split_line.first) << " tail : " << std::quoted(split_line.second);
+    int ix{};
+    for (auto split = SplitOn<','>{}.parse(split_line.first); !split.empty(); split = SplitOn<','>{}.parse(split[0].second)) {
+      std::cout << NT << NT << "head : " << std::quoted(split[0].first) << " tail : " << std::quoted(split[0].second);
+      result.back()[ix++] = std::stoi(std::string(split[0].first));
+    }
+    for (auto split = SplitOn<','>{}.parse(split_line.second); !split.empty(); split = SplitOn<','>{}.parse(split[0].second)) {
+      std::cout << NT << NT << "head : " << std::quoted(split[0].first) << " tail : " << std::quoted(split[0].second);
+      result.back()[ix++] = std::stoi(std::string(split[0].first));
+    }
+  }
+  return result;
+}
+
+void print_model(Model const& model) {
+  std::cout << NL << "Model";
+  for (auto const& entry : model) {
+    std::cout << NL << "px py pz vx vy vz : ";
+    for (auto const& value : entry) {
+      std::cout << " " << value;
+    }
+  }
+}
+
+namespace part1 {
+  Result solve_for(Model& model,auto args) {
+    Result result{};
+    std::cout << NL << NL << "part1";
+    print_model(model);
+    return result;
+  }
+}
+
+namespace part2 {
+  Result solve_for(Model& model,auto args) {
+    Result result{};
+    std::cout << NL << NL << "part2";
+    return result;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Solution solution{};
+  std::cout << NL << "argc : " << argc;
+  for (int i = 0; i < argc; ++i) {
+    std::cout << NL << "argv[" << i << "] : " << std::quoted(argv[i]);
+  }
+  // day00 x y
+  std::tuple<int,std::string> args{1,"example.txt"};
+  auto& [part,file] = args;
+  if (argc > 1 ) {
+    part = std::stoi(argv[1]);
+    if (argc > 2) {
+      file = argv[2];
+    }
+  }
+  std::cout << NL << "Part : " << part << " file : " << file;
+  std::ifstream in{ file };
+  auto model = parse(in);
+
+  switch (part) {
+  case 1: {
+    auto answer = part1::solve_for(model,args);
+    solution[part].push_back({ file,answer });
+  } break;
+  case 2: {
+    auto answer = part2::solve_for(model,args);
+    solution[part].push_back({ file,answer });
+  } break;
+  default:
+    std::cout << NL << "No part " << part << " only part 1 and 2";
+  }
+
+  std::cout << NL << NL << "------------ REPORT----------------";
+  for (auto const& [part, answers] : solution) {
+    std::cout << NL << "Part " << part << " answers";
+    for (auto const& [heading, answer] : answers) {
+      if (answer != 0) std::cout << NT << "answer[" << heading << "] " << answer;
+      else std::cout << NT << "answer[" << heading << "] " << " NO OPERATION ";
+    }
+  }
+  std::cout << NL << NL;
+  return 0;
+}
